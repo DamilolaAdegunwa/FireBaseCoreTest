132a374436a2f5bd56736102511e8f5fea017405
michael berezin <mbearz@gmail.com>
2017-12-30 22:43:43 +0200

    first check in

---
+++ FireBaseCoreTest/wwwroot/js/site.js
@@ -0,0 +1,107 @@
+﻿$(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAf3zX8rG4hGfjLQK4Er61bTW7cQjTptgE",
+        authDomain: "barontest-152eb.firebaseapp.com",
+        databaseURL: "https://barontest-152eb.firebaseio.com",
+        projectId: "barontest-152eb",
+        storageBucket: "barontest-152eb.appspot.com",
+        messagingSenderId: "592512603881"
+    };
+    firebase.initializeApp(config);
+
+    firebase.auth().signInAnonymously();
+
+    var usersListRef = firebase.database().ref('users_list');
+    //var newUserRef = usersListRef.push();
+
+    var conversationsRef = firebase.database().ref('conversations');
+    //var newConversationsRef = conversationsRef.push();
+
+    var userIitDone = false;
+    var chatIitDone = false;
+
+    var newUserRow = $(".newUserRow");
+    var usersList = $(".users");
+    var welcomeText = $(".welcomeText");
+    var chat = $(".fullChat");
+
+    $(".addUser").click(function () {
+        newUserRow.toggle();
+        $(this).html($(".newUserRow").is(":visible") ? "Cancel" : "Add User");
+    });
+
+    $(".saveUser").click(function () {
+        var name = $(".userNaem").val();
+        var id = usersList.find("li").length + 1;
+        usersListRef.push({ "id": id, "name": name });
+        var path = newUserRef.toString();
+    });
+
+    $(document).on("click", ".userItem", function () {
+        var id = $(this).attr("data-id");
+        var name = $(this).attr("data-name");
+        welcomeText.html("Welcome " + name).attr("data-user-id", id).attr("data-name", name);
+    });
+
+    usersListRef.once('value', function (snapshot) {
+        //read all of the users on page load
+        var val = snapshot.exportVal();
+        if (!val) return;
+
+        var i = 0;
+        for (var prop in val) {
+            var user = val[prop];
+            if (i === 0) {
+                welcomeText.html("Welcome " + user.name).attr("data-user-id", user.id).attr("data-name", user.name);
+                i++;
+            }
+            usersList.append(createUserLi(user.id, user.name));
+        }
+        userIitDone = true;
+    });
+
+    usersListRef.on('child_added', function (snapshot) {
+        if (!userIitDone) return;//get all the new users, as they are added
+        var val = snapshot.val();
+        usersList.append(createUserLi(val.id, val.name));
+        $(".addUser").click();
+    });
+
+    function createUserLi(id, name) {
+        return "<li class='userItem pointer' data-id='{0}' data-name='{1}'> {2}</li>".format(id, name, name);
+    }
+
+    $(".postText").click(function () {
+        var text = $(".text").val();
+        var name = welcomeText.attr("data-name");
+        var date = (new Date()).format("dd/MM/yyyy HH:mm");
+        conversationsRef.push({ text: text, username: name, date: date });
+        var path = newConversationsRef.toString();
+    });
+
+    conversationsRef.once('value', function (snapshot) {
+        //read all of the users on page load
+        var val = snapshot.exportVal();
+        if (!val) return;
+
+        var i = 0;
+        for (var prop in val) {
+            var message = val[prop];
+            createChatMessage(message.username, message.text, message.date);
+        }
+        chatIitDone = true;
+    });
+
+    conversationsRef.on('child_added', function (snapshot) {
+        if (!chatIitDone) return;//get all the new users, as they are added
+        var message = snapshot.val();
+        createChatMessage(message.username, message.text, message.date);
+    });
+
+    function createChatMessage(userName, text, date) {
+        chat.append("<div>{0}: {1} [{2}] </div>".format(userName, text, date));
+    }
+
+
+});
---
+++ FireSharp/Program.cs
@@ -0,0 +1,93 @@
+﻿using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Threading.Tasks;
+
+namespace FireSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    //        var config = {
+                    //    apiKey: "AIzaSyAf3zX8rG4hGfjLQK4Er61bTW7cQjTptgE",
+                    //    authDomain: "barontest-152eb.firebaseapp.com",
+                    //    databaseURL: "https://barontest-152eb.firebaseio.com",
+                    //    projectId: "barontest-152eb",
+                    //    storageBucket: "barontest-152eb.appspot.com",
+                    //    messagingSenderId: "592512603881"
+                    //};
+                    //"conversations"
+
+                    var apiKey = "AIzaSyAf3zX8rG4hGfjLQK4Er61bTW7cQjTptgE"; // your app secret
+
+                    var firebaseAuthProvider = new FirebaseAuthProvider(new FirebaseConfig(apiKey));
+
+                    var auth = await firebaseAuthProvider.SignInAnonymouslyAsync();
+
+                    var firebaseClient = new FirebaseClient(
+                      "https://barontest-152eb.firebaseio.com/",
+                      new FirebaseOptions
+                      {
+                          AuthTokenAsyncFactory = () => Task.FromResult(auth.FirebaseToken.ToString())
+                      });
+
+                    var conversations = firebaseClient.Child("conversations");
+
+                    var conversationsList = await conversations
+                      .OrderByKey()
+                      .LimitToFirst(2)
+                      .OnceAsync<ConverstionNode>();
+
+                    foreach (var conversation in conversationsList)
+                    {
+                        Console.WriteLine($"{conversation.Key} is {conversation.Object.ToString()}");
+                    }
+
+
+                    //// add new item to list of data and let the client generate new key for you (done offline)
+                    //var dino = await firebase
+                    //  .Child("dinosaurs")
+                    //  .PostAsync(new Dinosaur());
+
+                    //// note that there is another overload for the PostAsync method which delegates the new key generation to the firebase server
+
+                    //Console.WriteLine($"Key for the new dinosaur: {dino.Key}");
+
+                    //// add new item directly to the specified location (this will overwrite whatever data already exists at that location)
+                    //await firebase
+                    //  .Child("dinosaurs")
+                    //  .Child("t-rex")
+                    //  .PutAsync(new Dinosaur());
+
+                    //// delete given child node
+                    //await firebase
+                    //  .Child("dinosaurs")
+                    //  .Child("t-rex")
+                    //  .DeleteAsync();
+
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }).Wait();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ConverstionNode
+    {
+        public string text { get; set; }
+        public string username { get; set; }
+        public string date { get; set; }
+
+        public override string ToString() => $"{username}:{text} [{date}]";
+    }
+}
